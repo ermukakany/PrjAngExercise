da18fcfa1b4e211abdb16b451f0c9c5516dc3264
emukakan <ernestine.mukakanyenzi@capgemini.com>
2019-03-07 15:45:48 +0100

    le nouveau server

--- hell.js
+++ hell.js
@@ -4,3 +4,12 @@ var server = http.createServer(function(req, res) {
   res.end('Salut tout le monde !');
 });
 server.listen(8080);
+
+// Code identique au précédent
+
+var instructionsNouveauVisiteur = function(req, res) {
+  res.writeHead(200);
+  res.end('Salut tout le monde !');
+}
+
+var server1 = http.createServer(instructionsNouveauVisiteur);
